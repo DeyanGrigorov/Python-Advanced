0e56d104ee91ef0a6dd892dd5a32bca5e3ae99bd
Deyan Grigorov <deian1998@abv.bg>
2020-11-04 00:24:55 +0200

    Adding two new files

---
+++ OOP/Defining_Classes/exc_from_1_to_5/programmer.py
@@ -0,0 +1,22 @@
+class Programmer:
+    def __init__(self, name: str, language: str, skills: int):
+        self.name = name
+        self.language = language
+        self.skills = skills
+
+    def watch_course(self, course_name, language, skills_earned):
+        if self.language == language:
+            self.skills += skills_earned
+            return f"{self.name} watched {course_name}"
+        return f"{self.name} does not know {language}"
+
+    def change_language(self, new_language, skills_needed):
+        if self.skills < skills_needed:
+            return f"{self.name} needs {abs(skills_needed - self.skills)} more skills"
+        elif self.skills >= skills_needed and new_language != self.language:
+            previous_language = self.language
+            self.language = new_language
+            return f"{self.name} switched from {previous_language} to {new_language}"
+        elif self.skills >= skills_needed and new_language == self.language:
+            return f"{self.name} already knows {new_language}"
+
