8f0a1612d406b523574ddfc07e49c1d44207d54e
Deyan Grigorov <deian1998@abv.bg>
2020-11-19 17:29:06 +0200

    Adding new exercises for inheritance in  python

---
+++ OOP/Inheritance/Person/project/person.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+
+
---
+++ OOP/Inheritance/Zoo/project/animal.py
@@ -0,0 +1,11 @@
+class Animal:
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
